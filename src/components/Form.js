bd0358104ad17816dd430f698f79025f92957ecb
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-10-03 23:05:07 +0300

    completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,58 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+  const [formData, setFormData] = useState({
+    firstName:"John",
+    lastName:"Wangombe",
+    subscribe: true,
+  })
+
+  function handleChange(event) {
+    let  name = event.target.name;
+    let value = event.target.value;
+
+    if( event.target.type === "checkbox"){
+      value = event.target.checked
+    }
+
+    setFormData({
+      ...formData,
+      [name]:value,
+    })
   }
-
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
   }
 
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+    <form onSubmit={handleSubmit}>
+      <input type="text"
+      name="firstName" 
+      onChange={handleChange} 
+      value={formData.firstName} 
+      />
+
+
+      <input type="text" 
+      name="lastName" 
+      onChange={handleChange} 
+      value={formData.lastName} 
+      />
+
+      <br></br>
+
+      <label>
+        <input  
+        type="checkbox" 
+        name="subscribe" 
+        checked={formData.subscribe}
+        onChange={handleChange}
+        />
+        Subcribe to our channel
+      </label>
+      <br></br>
       <button type="submit">Submit</button>
     </form>
   );
